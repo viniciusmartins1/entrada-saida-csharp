8508f6f8e1abb7dfe592e29f097b935d0b675583
Vinicius Martins <vm3090474@gmail.com>
2021-09-27 23:33:26 -0300

    Deixando o projeto pronto para o curso

---
+++ ByteBank/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ByteBankImportacaoExportacao.Modelos;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ContaCorrente conta = new ContaCorrente(3421, 32141);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# entrada-saida-csharp
-C#: Entrada e Saída(I/O) com streams
+# C#: Entrada e Saída(I/O) com streams
+
+Repositório para exercitar o conteúdo do último curso da formação de C# do [Alura]{https://alura.com.br}.
+

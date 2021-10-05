9e378398a0c43f4d52f312289cbbba693dc2309b
Vinicius Martins <vm3090474@gmail.com>
2021-10-04 23:20:21 -0300

    Auxiliares na classe File

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -10,18 +10,38 @@ using ByteBankImportacaoExportacao.Modelos;
 namespace ByteBank
 {
     partial class Program
-    {
+    {   
+
         static void Main(string[] args)
         {
-            //EscritaBinaria();
-            //LeituraBinaria();
 
+            File.WriteAllText("escrevendoComAClasseFile.txt", "Testando File.WriteAllText");
+
+            var bytesArquivo = File.ReadAllBytes("contas.txt");
+
+            Console.WriteLine($"Arquivo contas.txt possui {bytesArquivo.Length}");
+
+
+            
+
+            var linhas = File.ReadAllLines("contas.txt");
+            Console.WriteLine(linhas.Length);
+
+            foreach (var linha in linhas)
+            {
+                Console.WriteLine(linha);
+            }
+
+            Console.ReadLine();
+
+
+            Console.WriteLine("Digite o seu nome:");
+            var nome = Console.ReadLine();
+
+            Console.WriteLine(nome);
+    
             UsarStreamDeEntrada();
             
-           //CriarArquivo();
-
-           //CriarArquivoComWriter();
-           //TesteEcrita();
            Console.WriteLine("Aplicação finalizada");
         }
        
